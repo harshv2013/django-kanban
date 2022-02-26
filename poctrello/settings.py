1f5b3ccca072ef5adce480ee6f5d9c688dff985f
harsh.vardhan <harsh.vardhan@impressico.com>
2022-02-26 22:10:44 +0530

    heroku updates

--- poctrello/settings.py
+++ poctrello/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default secret key')
 DEBUG = False
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'django-kanbanapp.herokuapp.com/']
 
 
 # Application definition
