594f89e6825ac2b83a570da53bda6cf5745d117b
harsh.vardhan <harsh.vardhan@impressico.com>
2022-02-27 13:40:00 +0530

    url chaged

--- poctrello/urls.py
+++ poctrello/urls.py
@@ -23,7 +23,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('kanban/', include('kanbanapp.urls')),
+    path('', include('kanbanapp.urls')),
     path('admin/', admin.site.urls),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
