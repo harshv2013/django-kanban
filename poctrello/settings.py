b10e5cea94f9a14712cc043a533e37c2c489652b
harsh.vardhan <harsh.vardhan@impressico.com>
2022-03-01 21:14:25 +0530

    user login created

--- poctrello/settings.py
+++ poctrello/settings.py
@@ -179,6 +179,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
 
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
