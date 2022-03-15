5289c041914a6a89e0b13edf883cac1a3a1aa1cf
harsh.vardhan <harsh.vardhan@impressico.com>
2022-03-15 20:14:19 +0530

    adder username with jwt

--- authuser/serializers.py
+++ authuser/serializers.py
@@ -7,13 +7,25 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
-    @classmethod
-    def get_token(cls, user):
-        token = super(MyTokenObtainPairSerializer, cls).get_token(user)
+    # @classmethod
+    # def get_token(cls, user):
+    #     token = super(MyTokenObtainPairSerializer, cls).get_token(user)
+    #
+    #     # Add custom claims
+    #     token['username'] = user.username
+    #     return token
 
-        # Add custom claims
-        token['username'] = user.username
-        return token
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        refresh = self.get_token(self.user)
+        data['refresh'] = str(refresh)
+        # data.pop('refresh', None) # remove refresh from the payload
+        data['access'] = str(refresh.access_token)
+
+        # Add extra responses here
+        data['user'] = self.user.username
+        # data['date'] = datetime.date.today()
+        return data
 
 
 ############################################################################
